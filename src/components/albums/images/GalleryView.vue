501bdd74e5bf7541c61e3ca04367cad60f023bed
Connor Frendt <cjacksonfx@yahoo.com>
2018-11-30 15:30:14 -0800

    set up different list views

---
+++ src/components/albums/images/GalleryView.vue
@@ -0,0 +1,51 @@
+<template>
+  <Modal :onClose="() => this.$router.back()">
+      <section>
+        <h3>Gallery View</h3>
+        {{selectedIndex}}
+        <p>
+          <button @click="setIndex(-1)">&lt;</button>
+          {{color}}
+          <button @click="setIndex(1)">&gt;</button>
+        </p>
+      </section>
+  </Modal>
+</template>
+
+<script>
+import Modal from '../../shared/Modal';
+
+export default {
+  data() {
+    return {
+      selectedIndex: 0
+    };
+  },
+  props: {
+    images: Array
+  },
+  components: {
+    Modal
+  },
+  computed: {
+    albums() {
+      return this.albums[this.selectedIndex];
+    }
+  },
+  methods: {
+    setIndex(amt) {
+      this.selectedIndex += amt;
+      if(this.selectedIndex === this.albums.length) {
+        this.selectedIndex = 0;
+      }
+      else if(this.selectedIndex < 0) {
+        this.selectedIndex = this.albums.length - 1;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/albums/images/ListView.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h3>List View</h3>
+    <p>There are {{images.length}} image(s)</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    images: Array
+  }
+};
+</script>
+
+<style scoped lang="postcss">
+
+</style>
