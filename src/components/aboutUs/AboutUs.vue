0f7bad5dd42a61cd9823596c7c18bd0f9c43948c
Connor Frendt <cjacksonfx@yahoo.com>
2018-11-30 14:16:13 -0800

    finished about us page

--- src/components/aboutUs/AboutUs.vue
+++ src/components/aboutUs/AboutUs.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
+  <div id="about">
     <h2>Connor</h2>
-    <p></p>
+    <img src="./../../assets/connor-in-peru.jpg" style="width: 200px">
+    <p>Mathematician, software dev, and lover of riddles!</p>
+    <hr />
     <h2>Marcy</h2>
-    <p></p>
+    <img src="./../../assets/Marcy.jpg" style="width: 100px">
+    <p>Software developer in progress! I love to read, workout, and pet dogs. Please add an adorable picture of your doggo.</p>
   </div>
 </template>
 
@@ -15,5 +18,10 @@ export default {
 </script>
 
 <style>
-
+#about {
+  background: lightgray;
+  border-radius: 5px;
+  padding: 10px;
+  margin-top: 10px;
+}
 </style>
