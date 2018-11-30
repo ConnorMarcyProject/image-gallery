9c5d5bdea2f3d9f4030dacc49c3b74d686893ea9
Marcy Silverman <marcysilve225@gmail.com>
2018-11-30 14:15:31 -0800

    modal set up

---
+++ src/components/albums/images/AddImage.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="onAdd(image)">
+    <FormControl label="Title">
+      <input v-model="image.title">
+    </FormControl>
+    <FormControl label="Url">
+      <input v-model="image.url">
+    </FormControl>
+    <FormControl>
+      <button>Add</button>
+    </FormControl>
+  </form>
+</template>
+
+<script>
+import FormControl from '../../../shared/FormControl'; 
+
+export default {
+  date() {
+    return {
+      image: {
+        title: '', 
+        url:''
+      }
+    };
+  }, 
+  props: {
+    onAdd: Function
+  }, 
+  components: {
+    FormControl
+  }
+
+};
+</script>
+
+<style>
+
+</style>
